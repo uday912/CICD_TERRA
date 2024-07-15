125c7c7e61d91fb43d53166dc92f1d3705896dca
uday912 <utripuraneni@gmail.com>
2024-07-15 19:05:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 module "ec2_instance" {
-  source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" 
+  source = "./modules_ec2instance"
+  ami_value = "ami-04a81a99f5ec58529" 
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". 
+  subnet_id_value = "subnet-096bbb546694fc83a". 
 }
