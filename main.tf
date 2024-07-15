a56ccfc1b03c962cc496427308c792230c6951c1
uday912 <utripuraneni@gmail.com>
2024-07-15 19:27:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ module "ec2_instance" {
   source = "./modules_ec2instance"
   ami_value = "ami-04a81a99f5ec58529" 
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-096bbb546694fc83a". 
+  subnet_id_value = "subnet-096bbb546694fc83a"
 }
